8b7d797535e744ec83ec9e483ff08ca67145201c
ev-indrik <94191315+ev-indrik@users.noreply.github.com>
2022-10-09 20:07:08 +0200

    favicon, city name table added

--- src/Table.js
+++ src/Table.js
@@ -5,18 +5,21 @@ import "./Table.css";
 export default function Table(props) {
   if (props.data) {
     return (
-      <ul className="Table">
-        <li>{`Temperature: ${Math.round(props.data.main.temp)}`}</li>
-        <li>{`Description: ${props.data.weather[0].main}`}</li>
-        <li>{`Humidity: ${props.data.main.humidity}%`}</li>
-        <li>{`Wind: ${Math.round(props.data.wind.speed)} km/h`}</li>
-        <li>
-          <img
-            src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
-            alt="icon"
-          />
-        </li>
-      </ul>
+      <div className="Table">
+        <h2>{`City: ${props.data.name}`}</h2>
+        <ul className="Table">
+          <li>{`Temperature: ${Math.round(props.data.main.temp)}`}</li>
+          <li>{`Description: ${props.data.weather[0].main}`}</li>
+          <li>{`Humidity: ${props.data.main.humidity}%`}</li>
+          <li>{`Wind: ${Math.round(props.data.wind.speed)} km/h`}</li>
+          <li>
+            <img
+              src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
+              alt="icon"
+            />
+          </li>
+        </ul>
+      </div>
     );
   } else {
     return (
