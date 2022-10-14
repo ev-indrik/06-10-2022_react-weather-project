b54ae0a357563bd49cf89fb7832688a90b017e87
ev-indrik <94191315+ev-indrik@users.noreply.github.com>
2022-10-14 16:07:36 +0200

    media queries added

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import "./Search.css";
 
 export default function Search(props) {
   function updateCity(event) {
